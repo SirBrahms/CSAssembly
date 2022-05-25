bb20de7e30111bd341d744bdec55aadd04f566b8
SirBrahms <95311570+SirBrahms@users.noreply.github.com>
2022-05-25 20:16:03 +0200

    Implement Registers and add Comments

--- CSAssembly.cs
+++ CSAssembly.cs
@@ -1,17 +1,30 @@
 namespace CSAssembly
 {
+    // Main Class
+    // Handles all the Assembly parsing and executing logic
     class AssemblyHandler
     {
-        public Register RegEAX = new Register("EAX");
-        public Register RegEBX = new Register("EBX");
-        public Register RegECX = new Register("ECX");
-        public Register RegEDX = new Register("EDX");
+        // Defining all the Registers and assigning their names
+        public static Register RegEAX = new Register("EAX"); // Accumulator
+        public static Register RegEBX = new Register("EBX"); // Base
+        public static Register RegECX = new Register("ECX"); // Counter
+        public static Register RegEDX = new Register("EDX"); // Data
+        public static Register RegEPI = new Register("EIP"); // Instruction Pointer
+        public static Register RegESP = new Register("ESP"); // Stack Pointer
+        public static Register RegEBP = new Register("EBP"); // Base Pointer (For Returning)
+        public static Register RegESI = new Register("ESI"); // Source Index for String Operations
+        public static Register RegEDI = new Register("EDI"); // Destination Index for String Operations
         
     }
 
-    struct Register 
+    #region Custom Types
+
+    // Implementation of a Register Class
+    // It has a string as a Name
+    // And it has a dynamic Value for the Register's contents
+    class Register 
     {
-        public string Name = "";
+        public readonly string Name = "";
         public dynamic Contents = 0;
 
         public Register(string Name, dynamic? Contents = default(dynamic)) {
@@ -20,4 +33,6 @@ namespace CSAssembly
             else this.Contents = -1; // Else it will be assigned a -1
         }
     }
+
+    #endregion
 }
