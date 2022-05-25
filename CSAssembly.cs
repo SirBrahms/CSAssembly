a9c74d9f4ee0d758d2262a1762ec40907765ef5b
SirBrahms <95311570+SirBrahms@users.noreply.github.com>
2022-05-25 20:40:18 +0200

    Implement Flags

--- CSAssembly.cs
+++ CSAssembly.cs
@@ -34,5 +34,67 @@ namespace CSAssembly
         }
     }
 
+    // Implementation of a Flag Class
+    // It contains all the Flags that are used in Assembly
+    // Cannot be instantiated (as it is static)
+    static class Flags
+    {
+        /* 
+        * It contains the carry of 0 or 1 from a high-order bit (leftmost)
+        * after an arithmetic operation. It also stores the contents of last bit of a shift or rotate operation. \/
+        */
+        public static int Carry = 0b0;
+
+        /*
+        * It contains the carry from bit 3 to bit 4 following an arithmetic operation; used for specialized arithmetic.
+        * The AF is set when a 1-byte arithmetic operation causes a carry from bit 3 into bit 4. \/
+        */
+        public static int AuxiliaryCarry = 0b0;
+
+        /*
+        * It indicates the overflow of a high-order bit (leftmost bit) of data after a signed arithmetic operation. \/
+        */
+        public static bool Overflow = false;
+
+        /*
+        * It determines left or right direction for moving or comparing string data. 
+        * When the DF value is false, the string operation takes left-to-right direction
+        * and when the value is set to true, the string operation takes right-to-left direction. \/
+        */
+        public static bool Direction = false; 
+
+        /*
+        * It determines whether the external interrupts like keyboard entry, etc., are to be ignored or processed. 
+        * It disables the external interrupt when the value is false and enables interrupts when set to true. \/
+        */
+        public static bool Interrupts = false;
+
+        /*
+        * Sets the Processor into "Single Step Mode" if set to true. 
+        * Can be used in debugging Programs, to step trough the instructions one at a time \/
+        */
+        public static bool Trap = false;
+
+        /*
+        * Sets the arithmetical Sign of an operation.
+        * If set to true: Negative Result (-)
+        * If set to false: Postitive Result (+) \/
+        */
+        public static bool Sign = false;
+
+        /*
+        * It indicates the result of a mathematical operation or comparison.
+        * If that result turns out to be zero, this flag will be set to true \/
+        */
+        public static bool Zero = false;
+
+        /*
+        * It indicates the total number of 1-bits in the result of a mathematical operation.
+        * An even number of 1-bits sets this Flag to false (i.E: 0b11110000)
+        * An odd number of 1-bits sets this Flag to true (i.E: 0b10000000)
+        */
+        public static bool Parity = false;
+    }
+
     #endregion
 }
