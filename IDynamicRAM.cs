bf051dba9e724481adf73aea298a50e9dbe3bd4d
SirBrahms <95311570+SirBrahms@users.noreply.github.com>
2022-06-13 20:42:58 +0200

    Change Null safety on IDynamicRAM ReadByte() and ReadBytes() Function

--- IDynamicRAM.cs
+++ IDynamicRAM.cs
@@ -6,13 +6,13 @@ namespace CSAssembly.Types
         public bool WriteByte(byte ByteToWrite);
         
         // Function to read one byte from a specified Address
-        public byte ReadByte(int Address);
+        public byte? ReadByte(int Address);
+
+        // Function to Read a range of bytes From Memory
+        public byte?[] ReadBytes(int From, int To);
 
         // Function to Write multiple bytes to the next Location that is big enough to fit all the Bytes chained together
         public bool WriteBytes(byte[] BytesToWrite);
-
-        // Function to Read a range of bytes From Memory
-        public byte[] ReadBytes(int From, int To);
 
         // Function to free Resources
         public bool Free(int From, int To);
