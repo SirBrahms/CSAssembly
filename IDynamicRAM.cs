1c47c2bda252d0e046c51717a343743acaf21340
SirBrahms <95311570+SirBrahms@users.noreply.github.com>
2022-06-13 18:44:47 +0200

    Update IDynamicRAM list to be able to take NULL-Bytes

--- IDynamicRAM.cs
+++ IDynamicRAM.cs
@@ -3,7 +3,7 @@ namespace CSAssembly.Types
     public interface IDynamicRAM
     {
         // List that Represents the RAM (add Private set in implementation)
-        public List<byte> RAM {get;}
+        public List<byte?> RAM {get;}
 
         // Function to write one Byte to the next free Position in memory
         public bool WriteByte(byte ByteToWrite);
